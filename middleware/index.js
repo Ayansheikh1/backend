e652f1187b599dd50acf051017eb5b6a4c07ff48
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-03-12 18:07:23 +0530

    learn midleware

--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const users = require("./routes/user")
+
+//simple logger mddleware
 function logger(req,res,next){
   console.log("Method: ",req.method);
   console.log("URL:",req.url);
@@ -10,6 +13,17 @@ function logger(req,res,next){
   next();
 
 }
+//Understand middleware applied to specific routes.
+
+function checkKey(req,res,next){
+  if(req.query.key === "123"){
+    next();
+  }
+  else{
+    res.send("access denied");
+  }
+}
+
 
 app.use(logger);
 
@@ -17,6 +31,24 @@ app.get('/', (req,res)=>{
   res.send("Home Page");
 })
 
+app.get('/admin',checkKey, (req,res)=>{
+  res.send("admin Page")
+})
+
+//routes response json
+app.get('/student', (req,res) => {
+  res.send([
+    { id:1, name: "ayan" },
+    { id:2, name: "ashar" }
+  ])
+})
+
+
+//
+app.use('/users', users);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
---
+++ middleware/routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.send("User list");
+});
+
+module.exports = router;
+// Create a separate file for routes.
