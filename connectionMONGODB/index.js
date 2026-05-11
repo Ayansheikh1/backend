2ecca198c21815c113044ff66690ef7f8b226322
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-05-12 00:41:02 +0530

    connect to mongodb using mongoose

--- connectionMONGODB/index.js
+++ connectionMONGODB/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const connectDB = require("./db");
+
+//connect to database
+connectDB();
+
+app.get('/', (req,res)=>{
+    console.log("we are inside the handler");
+    res.send("welcome dosto!");
+})
 
 app.listen(port,() =>{
  console.log("server running");
