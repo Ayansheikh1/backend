c796115805805f46ac453b4a1f967b437a22cc02
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-05-13 22:02:19 +0530

    update operation

--- connectionMONGODB/index.js
+++ connectionMONGODB/index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const connectDB = require("./db");
+const users = require("./routes/users");
+
+
+app.use(express.json());
 
 //connect to database
 connectDB();
+
+
+app.use('/api',users);
 
 app.get('/', (req,res)=>{
     console.log("we are inside the handler");
---
+++ connectionMONGODB/routes/users.js
@@ -0,0 +1,84 @@
+const express = require("express");
+ 
+const router = express.Router();
+
+const User = require("../models/userModel")
+
+
+//routes
+
+//CRUD operation
+
+
+//read/view
+
+router.get('/users', async (req,res) =>{
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+    }
+
+});
+
+//create
+router.post('/users', async (req,res) =>{
+    try {
+      const{name, age, weight} = req.body;
+      const newUser = new User({name, age, weight});
+      await newUser.save();//save to model
+
+      res.status(200).json({
+        success:true,
+        user:newUser
+      })
+      
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+    }
+
+});
+
+
+//update
+
+router.put('/users/:id',async(req,res) =>{
+    const{id} = req.params;
+    const{name,age,weight} = req.body;
+
+    try{
+        const updatedUser = User.findByIdAndUpdate(id,{name,age,weight});
+
+        if(!updatedUser){
+            res.json({
+                message: "user not found..."
+            });
+            
+        }
+
+        res.status(200).json({
+            success:true,
+            user:updatedUser
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+    }
+})
+
+
+
+
+
+
+module.exports = router;
